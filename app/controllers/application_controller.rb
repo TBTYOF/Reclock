4f87418cca24a71969178274ea7675936a51900f
TBTYOF <toribatyof@gmail.com>
2019-09-09 01:42:22 +0900

    Add after_sign_out_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+
 	def after_sign_in_path_for(resource)
     if resource.class == Admin
       admins_path
@@ -6,6 +7,16 @@ class ApplicationController < ActionController::Base
       root_path
     else
     	on_store_users_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    if resource == :admin
+      new_admin_session_path
+    elsif resource == :user
+      root_path
+    else
+    	new_on_store_session_path
     end
   end
 end
