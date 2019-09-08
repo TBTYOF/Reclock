d98876a2cb811a07dfc3c94a30e3a5da5294c80e
TBTYOF <toribatyof@gmail.com>
2019-09-08 21:20:07 +0900

    Remove email column to users & on_store_users

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,23 @@
 Rails.application.routes.draw do
-  namespace :users do
-    get 'users/edit'
-    get 'users/update'
-    get 'users/show'
-  end
-  devise_for :admins
+	root 'users/users#home'
+
+	devise_for :admins
   devise_for :on_store_users
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :users do
+    resources :users ,only:[:show, :edit, :update] do
+    	get '/about' => 'users#about', as: 'about'
+  		resource :delivery_addresses, only:[:create, :update, :destroy]
+  		resources :carts, only:[:create,:index,:update, :destroy]
+      get '/orders/buy' => 'orders#new', as: 'orders_new'
+  		resources :inquiries, only:[:new, :create]
+  		resources :replies, only:[:new, :create]
+  		resources :reviews, except:[:index, :show]
+  	end
+  	resources :on_store_users, only:[:index, :show] do
+	 		resources :favorites, only:[:create, :destroy]
+	  end
+  end
 end
--- db/migrate/20190908064238_devise_create_users.rb
+++ db/migrate/20190908064238_devise_create_users.rb
@@ -35,7 +35,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :name_kana
       t.string :telephone_number
-      t.string :email
       t.string :postal_code
       t.string :address
       t.boolean :is_quit, default: false, null: false
