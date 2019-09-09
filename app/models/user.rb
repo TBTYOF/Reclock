015ab621723c06d15d57cf123694d49c9e05bbda
TBTYOF <toribatyof@gmail.com>
2019-09-09 20:14:47 +0900

    Add  validation to user sing_up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :name_kana, presence: true
+  validates :telephone_number, presence: true
+  validates :postcode, presence: true
+  validates :address, presence: true
 end
