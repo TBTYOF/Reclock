8e38063ccf7fe8a433db53a7297bbb9698dead81
TBTYOF <toribatyof@gmail.com>
2019-09-16 18:39:31 +0900

    fix orders/edit

--- app/controllers/on_store_users/orders_controller.rb
+++ app/controllers/on_store_users/orders_controller.rb
@@ -1,18 +1,32 @@
 class OnStoreUsers::OrdersController < ApplicationController
 	def index
-		
 	end
 
 	def show
 		@order = Order.find(params[:id])
 		@index = params[:index]
+
+		respond_to do |format|
+	  	format.html
+	  	format.js
+	  end
 	end
 
 	def edit
-		
+		@order = Order.find(params[:id])
+
+		respond_to do |format|
+	  	format.html
+	  	format.js
+	  end
 	end
 
 	def update
+		binding.pry
 		
+		respond_to do |format|
+	  	format.html
+	  	format.js
+	  end
 	end
 end
