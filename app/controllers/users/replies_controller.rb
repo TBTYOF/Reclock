598625b5a7833de2d8ea4db59b9745ed5399b283
TBTYOF <toribatyof@gmail.com>
2019-09-17 15:48:06 +0900

    Update inquiry form

--- app/controllers/users/replies_controller.rb
+++ app/controllers/users/replies_controller.rb
@@ -2,13 +2,15 @@ class Users::RepliesController < ApplicationController
 	def new
 		@reply = Reply.new
 		@inquiry = Inquiry.find(params[:inquiry_id])
+		@reply_creat_url = "/users/users/#{current_user.id}/inquiries/#{@inquiry.id}/replies"
 		if params[:order_id].present?
 			@order = Order.find(params[:order_id])
+			@reply_creat_url = "/users/users/#{current_user.id}/orders/#{@order.id}/inquiries/#{@inquiry.id}/replies"
 		end
 
 		respond_to do |format|
 	  	format.html
-	  	format.js
+	  	format.js {render '/shared/replies/new.js.erb'}
 	  end
 	end
 
@@ -22,21 +24,22 @@ class Users::RepliesController < ApplicationController
 
 		if params[:order_id] != nil
 			@order = Order.find(params[:order_id])
+			@reply_new_url = "/users/users/#{current_user.id}/orders/#{@order.id}/inquiries/#{@inquiry.id}/replies/new"
 
 			respond_to do |format|
 		  	format.html
-		  	format.js {render '/users/replies/show.js.erb'}
+		  	format.js {render '/shared/replies/show.js.erb'}
 		  end
 		else
+			@reply_new_url = "/users/users/#{current_user.id}/inquiries/#{@inquiry.id}/replies/new"
 			respond_to do |format|
 		  	format.html
-		  	format.js {render '/users/replies/show.js.erb'}
+		  	format.js {render '/shared/replies/show.js.erb'}
 		  end
 		end
 	end
 
 	def show
-		
 	end
 
 	private
