8ee4817ac228d74b6bac2b8c08223a30f1f793de
TBTYOF <toribatyof@gmail.com>
2019-09-18 16:24:09 +0900

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -112,16 +112,102 @@ end
 
 # 注文前問合せを生成
 User.all.length.times do |n|
-	3.times do |i| # 各ユーザ毎に3つディスク持つ
+	3.times do |i| # 各ユーザ毎に3つ持つ
 		title = "title-#{n+1}"
 		body = "body-#{(n+1)}"*100
 		flag = %w[true false]
-		Review.create!( user_id: rand(1..User.all.length),
+		Inquiry.create!( user_id: rand(1..User.all.length),
 										on_store_user_id: rand(1..OnStoreUser.all.length),
 										title: title,
 										body: body,
 										user_read: flag[rand(0..1)],
 										on_store_user_read: flag[rand(0..1)]
 								  )
+	end
+end
+
+# テスト出店者呼び出し
+shop_id = OnStoreUser.first.id
+
+# 注文を生成
+User.all.length.times do |n|
+	user = User.find(n+1)
+	maker = %w[CASIO CITIZEN OMEGA ROLEX 川村時計 源 サンプルクロックス 友村商事]
+	model = %w[RX-78 ZGMF-X10A X-18999 M-72]
+	symptom = "サンプル　"
+	symptom = "#{symptom * 20}"
+	2.times do |i| # 各ユーザ毎に3つ持つ
+		Order.create!( user_id: user.id,
+										on_store_user_id: shop_id,
+										variety: rand(1..7),
+										pickup: rand(1..4),
+										maker: maker[rand(0..7)],
+										model: model[rand(0..3)],
+										symptom: symptom,
+										repair_status: rand(1..8),
+										repair_detail: symptom,
+										payment: rand(1..3),
+										charge: 50000,
+										delivery: rand(1..4),
+										delivery_day: DateTime.now
+								  )
+	end
+end
+
+# 注文後問合せを生成
+Order.all.length.times do |n|
+	order = Order.find(n+1)
+	title = "title-#{n+1}"
+	body = "body-#{(n+1)}"*100
+	flag = %w[true false]
+	Inquiry.create!( user_id: order.user_id,
+									on_store_user_id: shop_id,
+									order_id: order.id,
+									title: title,
+									body: body,
+									user_read: flag[rand(0..1)],
+									on_store_user_read: flag[rand(0..1)]
+							  )
+end
+
+# レビューを生成
+Order.all.length.times do |n|
+	order = Order.find(n+1)
+	title = "title-#{n+1}"
+	body = "body-#{(n+1)}"*100
+	Review.create!( user_id: order.user_id,
+									on_store_user_id: shop_id,
+									order_id: order.id,
+									title: title,
+									body: body
+							  )
+end
+
+# サービス概要を生成
+OnStoreUser.all.length.times do |n|
+		on_store_user = OnStoreUser.find(n+1)
+		name = %w[ゼンマイ式(一般国産) ゼンマイ式(舶来) 重錘式(ホールクロック) 全般]
+		MajorCategory.create!(on_store_user_id: on_store_user.id,
+													name: name[rand(0..3)]
+								  			)
+end
+MajorCategory.all.length.times do |n|
+	major_category = MajorCategory.find(n+1)
+	2.times do |i| # 各ユーザ毎に3つ持つ
+		name = %w[クォーツムーブ交換 目覚まし時計 掛、置時計 チャイム付き(メロディ) 国産(時打ちのみ)]
+		MiddleCategory.create!(major_category_id: major_category.id,
+														name: name[rand(0..4)]
+									  			)
+	end
+end
+MiddleCategory.all.length.times do |n|
+	middle_category = MiddleCategory.find(n+1)
+	3.times do |i| # 各ユーザ毎に3つ持つ
+		name = %w[分解掃除 部品交換 ゼンマイ交換 部品新規作成 振り子付き 時報付き 通常時計のみ]
+		quote = %w[7,000円 15,000円 プラス4,000 30,000円 50,000円 別途見積もり]
+		MinorCategory.create!(middle_category_id: middle_category.id,
+														name: name[rand(0..6)],
+														quote: quote[rand(0..5)]
+									  			)
 	end
 end
