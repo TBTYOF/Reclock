b1680d487c12f88a3c8c8b312b82121d76429b82
TBTYOF <toribatyof@gmail.com>
2019-09-20 11:21:22 +0900

    Add validation to review and inquiry and reply

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,3 +1,7 @@
 class Reply < ApplicationRecord
 	belongs_to :inquiry
+
+  validates :body,
+            presence: true,
+            length: { maximum: 1000, message: "1000文字以内で入力して下さい"}
 end
--- db/migrate/20190908072622_create_reviews.rb
+++ db/migrate/20190908072622_create_reviews.rb
@@ -4,8 +4,8 @@ class CreateReviews < ActiveRecord::Migration[5.2]
     	t.references :user, foreign_key: true
     	t.references :on_store_user, foreign_key: true
     	t.references :order, foreign_key: true
-			t.string :title
-			t.text :body
+			t.string :title, null: false
+			t.text :body, null: false
 
       t.timestamps
     end
