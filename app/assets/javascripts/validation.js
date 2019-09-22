cab9e0def4ee9c7dae111a4448af47de24c8ba6c
TBTYOF <toribatyof@gmail.com>
2019-09-23 03:02:22 +0900

    Update inquiry/show, new, index

---
+++ app/assets/javascripts/validation.js
@@ -0,0 +1,45 @@
+$(function() {
+    $(document).on('click', 'input[name=edit_review]', function() {
+    	// バリデーション
+      if ($('#review_title').val() == '') {
+      	alert('未入力の項目があります');
+      	return false;
+      }
+      if ($('#review_body').val() == '') {
+      	alert('未入力の項目があります');
+      	return false;
+      }
+    })
+
+    $(document).on('click', 'input[name = new_review]', function() {
+    	// バリデーション
+      if ($('#review_title').val() == '') {
+      	alert('タイトルが未入力です。');
+      	return false;
+      }
+      if ($('#review_body').val() == '') {
+      	alert('本文が未入力です');
+      	return false;
+      }
+    })
+
+    $(document).on('click', 'input[name = new_inquiry]', function() {
+    	// バリデーション
+      if ($('#inquiry_title').val() == '') {
+      	alert('未入力の項目があります');
+        return false;
+      }
+      if ($('#inquiry_body').val() == '') {
+      	alert('未入力の項目があります');
+        return false;
+      }
+    })
+
+    $(document).on('click', 'input[name = new_riply]', function() {
+    	// バリデーション
+      if ($('#reply_body').val() == '') {
+      	alert('未入力の項目があります');
+        return false;
+      }
+    })
+  })
