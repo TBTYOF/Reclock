e64ff8952ebb696c6b86bd8c02fd4136a56035c4
TBTYOF <toribathof@gmail.com>
2019-10-25 17:50:19 +0900

    release versiton 1.4

--- app/mailers/on_store_user_mailer.rb
+++ app/mailers/on_store_user_mailer.rb
@@ -10,9 +10,9 @@ class OnStoreUserMailer < ApplicationMailer
 		on_store_user = params[:on_store_user]
 		@inquiry = params[:inquiry]
 		# テスト
-		@url = "http://localhost:3000/on_store_users/on_store_users/#{on_store_user.id}/inquiries"
+		# @url = "http://localhost:3000/on_store_users/on_store_users/#{on_store_user.id}/inquiries"
 		# デプロイ時はコメントアウトを外す
-		# @url = "http://reclock.work/on_store_users/on_store_users/#{on_store_user.id}/inquiries"
+		@url = "http://reclock.work/on_store_users/on_store_users/#{on_store_user.id}/inquiries"
 		mail(to: on_store_user.email, subject: '新着お問合せのお知らせ')
 	end
 
@@ -20,40 +20,40 @@ class OnStoreUserMailer < ApplicationMailer
 		@inquiry = params[:inquiry]
 		@reply = params[:reply]
 		# テスト
-		@url = "http://localhost:3000/on_store_users/on_store_users/#{@inquiry.on_store_user.id}/inquiries"
+		# @url = "http://localhost:3000/on_store_users/on_store_users/#{@inquiry.on_store_user.id}/inquiries"
 		# デプロイ時はコメントアウトを外す
-		# @url = "http://reclock.work/on_store_users/on_store_users/#{@inquiry.on_store_user.id}/inquiries"
+		@url = "http://reclock.work/on_store_users/on_store_users/#{@inquiry.on_store_user.id}/inquiries"
 		mail(to: @inquiry.on_store_user.email, subject: 'お問合せに返信が届いています')
 	end
 
 	def new_order
 		@order = params[:order]
 		# テスト
-		@order_index_url = "http://localhost:3000/on_store_users/on_store_users/#{@order.on_store_user.id}/orders"
-		@order_show_url = "http://localhost:3000/on_store_users/on_store_users/#{@order.on_store_user.id}/orders/#{@order.id}"
+		# @order_index_url = "http://localhost:3000/on_store_users/on_store_users/#{@order.on_store_user.id}/orders"
+		# @order_show_url = "http://localhost:3000/on_store_users/on_store_users/#{@order.on_store_user.id}/orders/#{@order.id}"
 		# デプロイ時はコメントアウトを外す
-		# @order_index_url = "http://reclock.work/on_store_users/on_store_users/#{@order.on_store_user.id}/orders"
-		# @order_show_url = "http://reclock.work/on_store_users/on_store_users/#{@order.on_store_user.id}/orders/#{@order.id}"
+		@order_index_url = "http://reclock.work/on_store_users/on_store_users/#{@order.on_store_user.id}/orders"
+		@order_show_url = "http://reclock.work/on_store_users/on_store_users/#{@order.on_store_user.id}/orders/#{@order.id}"
 		mail(to: @order.on_store_user.email, subject: '新規注文のお知らせ')
 	end
 
 	def update_order
 		@order = params[:order]
 		# テスト
-		@order_index_url = "http://localhost:3000/on_store_users/on_store_users/#{@order.on_store_user.id}/orders"
-		@order_show_url = "http://localhost:3000/on_store_users/on_store_users/#{@order.on_store_user.id}/orders/#{@order.id}"
+		# @order_index_url = "http://localhost:3000/on_store_users/on_store_users/#{@order.on_store_user.id}/orders"
+		# @order_show_url = "http://localhost:3000/on_store_users/on_store_users/#{@order.on_store_user.id}/orders/#{@order.id}"
 		# デプロイ時はコメントアウトを外す
-		# @order_index_url = "http://reclock.work/on_store_users/on_store_users/#{@order.on_store_user.id}/orders"
-		# @order_show_url = "http://reclock.work/on_store_users/on_store_users/#{@order.on_store_user.id}/orders/#{@order.id}"
+		@order_index_url = "http://reclock.work/on_store_users/on_store_users/#{@order.on_store_user.id}/orders"
+		@order_show_url = "http://reclock.work/on_store_users/on_store_users/#{@order.on_store_user.id}/orders/#{@order.id}"
 		mail(to: @order.on_store_user.email, subject: '支払方法更新のお知らせ')
 	end
 
 	def new_review
 		@review = params[:review]
 		# テスト
-		@url = "http://localhost:3000/on_store_users/on_store_users/#{@review.on_store_user.id}/reviews"
+		# @url = "http://localhost:3000/on_store_users/on_store_users/#{@review.on_store_user.id}/reviews"
 		# デプロイ時はコメントアウトを外す
-		# @url = "http://reclock.work/on_store_users/on_store_users/#{@review.on_store_user.id}/reviews"
+		@url = "http://reclock.work/on_store_users/on_store_users/#{@review.on_store_user.id}/reviews"
 		mail(to: @review.on_store_user.email, subject: 'レビュー投稿のお知らせ')
 	end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,9 +3,9 @@ class UserMailer < ApplicationMailer
 		user = params[:user]
 		@inquiry = params[:inquiry]
 		# テスト
-		@url = "http://localhost:3000/users/users/#{user.id}"
+		# @url = "http://localhost:3000/users/users/#{user.id}"
 		# デプロイ時はコメントアウトを外す
-		# @url = "http://reclock.work/users/users/#{user.id}"
+		@url = "http://reclock.work/users/users/#{user.id}"
 		mail(to: user.email, subject: '新着お問合せのお知らせ')
 	end
 
@@ -13,18 +13,18 @@ class UserMailer < ApplicationMailer
 		@inquiry = params[:inquiry]
 		@reply = params[:reply]
 		# テスト
-		@url = "http://localhost:3000/users/users/#{@inquiry.user.id}"
-		# デプロイ時はコメントアウトを外す
 		# @url = "http://localhost:3000/users/users/#{@inquiry.user.id}"
+		# デプロイ時はコメントアウトを外す
+		@url = "http://reclock.work/users/users/#{@inquiry.user.id}"
 		mail(to: @inquiry.user.email, subject: 'お問合せに返信が届いています')
 	end
 
 	def update_order
 		@order = params[:order]
 		# テスト
-		@url = "http://localhost:3000/users/users/#{@order.user.id}"
+		# @url = "http://localhost:3000/users/users/#{@order.user.id}"
 		# デプロイ時はコメントアウトを外す
-		# @url = "http://localhost:3000/users/users/#{@inquiry.user.id}"
+		@url = "http://reclock.work/users/users/#{@inquiry.user.id}"
 		mail(to: @order.user.email, subject: '修理状況が更新されました')
 	end
 end
